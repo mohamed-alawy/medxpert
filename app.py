f167d3481ef22c2678ec32ae7a16c06be33ce307
Mohamedalawy <mohamed.alawy.21@gmail.com>
2025-04-20 11:14:39 +0200

    fix dynamic port for Render

--- app.py
+++ app.py
@@ -417,4 +417,6 @@ def predict_fracture():
         'message': 'Invalid file format. Please upload a valid image file.'
     })
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
