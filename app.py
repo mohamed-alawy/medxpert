79e8ccaa9a41d173a579a5ea7fd9c7721116bb57
Mohamedalawy <mohamed.alawy.21@gmail.com>
2025-04-20 11:34:28 +0200

    Fix port

--- app.py
+++ app.py
@@ -418,5 +418,5 @@ def predict_fracture():
     })
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
